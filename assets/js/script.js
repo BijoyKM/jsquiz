0e79e1b0433a37aedc2a15e9a331b97d11f936b0
BijoyKM <bijoy.k.mathew@outlook.com>
2022-09-25 07:33:24 +0000

    Add functions for showing Question and Answers for the Quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,23 +7,24 @@
 // } else  alert("Please fill out the name field.");
 
 // }
-const startButn = document.getElementById('startBtn')
-const questionAreaElem = document.getElementById('questionArea')
+const startButn = document.querySelector('#startBtn')
+const nextButn = document.querySelector('#nextBtn')
+const questionAreaElem = document.querySelector('#questionArea')
 let questionsShuffle, currentQuesNum
-const quizQuesElem = document.getElementById('question')
-const quizAnsElem = document.getElementById('answerArea')
+const quizQuesElem = document.querySelector('#question')
+const quizAnsElem = document.querySelector('#answerArea')
 
 
 
 
 startButn.addEventListener('click', startQuiz)
 
-function getQuestions (count) {
-    for (var i = 0; i < count; i++) {
-        console.log(questions[i]);
-        //Do something
-    }
-}
+// function getQuestions (count) {
+//     for (var i = 0; i < count; i++) {
+//         console.log(questions[i]);
+//         //Do something
+//     }
+// }
 
 function startQuiz () {
 console.log("Quiz is started")
@@ -35,14 +36,34 @@ getNextQuestion ()
 }
 
 function getNextQuestion() {
+    clearState()
 showQuizQues(questionsShuffle[currentQuesNum])
 }
 
 function showQuizQues (question) {
     quizQuesElem.innerText = question.question
+    question.answers.forEach(answer => {
+        const answerButton = document.createElement('button')
+        answerButton.innerText = answer.choice
+        answerButton.classList.add('btn')
+        if(answer.is_correct) {
+            answerButton.dataset.correct = answer.is_correct
+        }
+        answerButton.addEventListener('click', answerSelect)
+        quizAnsElem.appendChild(answerButton)
+    })
 }
 
+function answerSelect(event) {
 
+}
+
+function clearState () {
+    nextButn.classList.add("hide")
+    while (quizAnsElem.firstChild) {
+        quizAnsElem.removeChild(quizAnsElem.firstChild)
+    }
+}
 
 
 
